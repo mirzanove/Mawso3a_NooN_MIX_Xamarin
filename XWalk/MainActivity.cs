b71b6b288641eb08b9d3b3adbf13208390883a00
mirzanove <mirzanove@gmail.com>
2019-01-20 08:20:01 +0200

    eeeeeeeeeeeeeeee

--- XWalk/MainActivity.cs
+++ XWalk/MainActivity.cs
@@ -16,11 +16,11 @@ using Org.Xwalk.Core;
 
 namespace XWalk
 {
-    [Activity(Label = "XWalk", MainLauncher = true)]
-    public class MainActivity : XWalkActivity
+    [Activity(Label = "@string/app_name", MainLauncher = true)]
+    public class MainActivity : Org.Xwalk.Core.XWalkActivity
     {
 
-        public static Activity fa;
+  
         public Org.Xwalk.Core.XWalkView xwv;
 
         protected override void OnCreate(Bundle savedInstanceState)
@@ -29,20 +29,24 @@ namespace XWalk
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            xwv = (Org.Xwalk.Core.XWalkView)FindViewById(Resource.Id.xwalkview);
         }
 
         protected override void OnXWalkReady()
         {
-            fa = this;
+            
 
-            var view = new RelativeLayout(this.BaseContext);
-            var mp = ViewGroup.LayoutParams.MatchParent;
-            xwv = new Org.Xwalk.Core.XWalkView(this.BaseContext, this);
-            view.AddView(xwv);
-            this.AddContentView(view, new ViewGroup.LayoutParams(mp, mp));
-            xwv.SetResourceClient(new XWalkResourceClient(xwv));
-            xwv.SetUIClient(new XWalkUIClient(xwv));
-            xwv.LoadUrl("file:///android_asset/index.html");
+            /* var view = new RelativeLayout(this.BaseContext);
+             var mp = ViewGroup.LayoutParams.MatchParent;
+             xwv = new Org.Xwalk.Core.XWalkView(this.BaseContext, this);
+             view.AddView(xwv);
+             this.AddContentView(view, new ViewGroup.LayoutParams(mp, mp));
+             xwv.SetResourceClient(new XWalkResourceClient(xwv));
+             xwv.SetUIClient(new XWalkUIClient(xwv));
+             xwv.LoadUrl("file:///android_asset/index.html");*/
+
+            xwv.Load("file:///android_asset/index.html", null);
 
         }
 
